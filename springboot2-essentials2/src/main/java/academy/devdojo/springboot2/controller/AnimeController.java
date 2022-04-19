53d9959bbc718e35292466e2ebfa01f39528edb4
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-04-19 18:59:08 -0300

    Criando método get que encontra um anime pelo seu id

--- springboot2-essentials2/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ springboot2-essentials2/src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +26,13 @@ public class AnimeController {
     private final AnimeService animeService;
 
     @GetMapping
-    public List<Anime> list() {
+    public ResponseEntity<List<Anime>> list() {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable Long id) {
+        return new ResponseEntity<>(animeService.FindById(id), HttpStatus.OK);
     }
 }
